413a2f4548f213538f4a0ba4610e24a891248042
kangyu <kyuang@126.com>
2021-01-07 16:58:31 +0800

    预约

--- app/index/controller/Product.php
+++ app/index/controller/Product.php
@@ -62,16 +62,82 @@ class Product extends CommonController
         $this->title = '明细';
 
         $id = input('id');
-        $detail = $this->app->db->name($this->table)->where(['id'=>$id])->find();
-        $detail['slider'] = empty($detail['slider']) ? [] : explode('|',$detail['slider']);
+        $detail = $this->app->db->name($this->table)->where(['id' => $id])->find();
+        $detail['slider'] = empty($detail['slider']) ? [] : explode('|', $detail['slider']);
 
         $cat_id = $detail['cate'];
         $cates = $this->app->db->name('ShopGoodsCate')->where(['id' => $cat_id])->find();
         $detail['cat_name'] = $cates['name'];
 
 //        dump($detail);die;
-        $this->assign('detail',$detail);
+        $this->assign('detail', $detail);
+        $this->assign('is_show', 0);
         $this->fetch('details');
+    }
+
+
+    public function yuyue()
+    {
+        $this->title = '预约';
+
+        $id = input('id');
+        $product = $this->app->db->name($this->table)->where(['id' => $id])->find();
+
+        $this->assign('product', $product);
+        $this->assign('is_show', 0);
+        $this->fetch('yuyue');
+    }
+
+
+    /**
+     * 保存在线报名记录
+     */
+    public function yuyue_store()
+    {
+        $product_id = input('product_id');
+        $mobile = input('mobile');
+        $username = input('username');
+        $message = input('message');
+
+        $product = $this->app->db->name($this->table)->where(['id' => $product_id])->find();
+        if (empty($product)) {
+            $this->error('预约课程错误！');
+        }
+
+        if (empty($mobile)) {
+            $this->error('请输入手机号！');
+        }
+        if (!isMobile($mobile)) {
+            $this->error('手机号格式错误！');
+        }
+        if (empty($username)) {
+            $this->error('请输入姓名！');
+        }
+        if (empty($message)) {
+            $this->error('请输入内容！');
+        }
+        $add_res = $this->app->db->name('DataGoodsApply')
+            ->where('mobile', $mobile)
+            ->where('product_id', $product_id)
+            ->where('status', 0)
+            ->find();
+        if (!empty($add_res)) {
+            $this->error('您已预约，请勿重复预约！');
+        }
+        $data = [
+            'product_id' => $product_id,
+            'mobile' => $mobile,
+            'username' => $username,
+            'message' => $message,
+            'create_at' => date('Y-m-d H:i:s')
+        ];
+        $add_res = $this->app->db->name('DataGoodsApply')->insertGetId($data);
+        if (!$add_res) {
+            $this->error('添加失败，请稍后重试！');
+        }
+        $this->app->db->name('ShopGoods')->where('id', $product_id)
+            ->update(['stock_sales' => $product['stock_sales'] + 1]);
+        return $this->success('添加成功！');
     }
 
 }
